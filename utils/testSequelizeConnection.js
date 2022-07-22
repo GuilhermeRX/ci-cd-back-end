c904b6c59652ec561629ba88bf917f39608d23ef
Luiz Guilherme <guilhermerx18@gmail.com>
2022-07-22 11:48:45 -0300

    deletando .env

---
+++ utils/testSequelizeConnection.js
@@ -0,0 +1,21 @@
+require('dotenv/config')
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize({
+  "username": process.env.DB_USER,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_DATABASE,
+  "host": process.env.DB_HOST,
+  "dialect": "postgres"
+});
+
+const testConection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+testConection();
