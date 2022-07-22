8b051903d46e545b51bd3d42bed9e68549b7467b
Luiz Guilherme <guilhermerx18@gmail.com>
2022-07-22 11:13:26 -0300

    add sequelize

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+require('dotenv/config')
 const express = require('express');
+const port = process.env.PORT || 3000;
+const { Product } = require('./models');
 
 const app = express();
-const port = process.env.PORT;
+app.use(express.json());
 
+
+app.post('/products', async (req, res) => {
+  const { name } = req.body;
+  const product = await Product.create({ name });
+  res.status(201).json(product);
+});
+
+app.get('/products', async (req, res) => {
+  const products = await Product.findAll();
+  res.status(200).json(products);
+});
 
 app.get('/', (_req, res) => {
   res.send('Hello Back End');
